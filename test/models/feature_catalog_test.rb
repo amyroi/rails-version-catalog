ddad95fc69f317d61119fd16179fe461e4e3d269
Atsuko Mori <1243446+amyroi@users.noreply.github.com>
2026-04-10 17:34:27 +0900

    fix: tighten auth metadata files

--- test/models/feature_catalog_test.rb
+++ test/models/feature_catalog_test.rb
@@ -34,6 +34,7 @@ class FeatureCatalogTest < ActiveSupport::TestCase
     assert_equal [ "app/models/user.rb", "app/models/session.rb", "app/controllers/sessions_controller.rb", "app/controllers/passwords_controller.rb" ], feature.files_for("8.0")
     assert_equal [ "bin/rails generate authentication" ], feature.code_examples_for("7.0")
     assert_equal [ "Confirm password reset mail delivery and signed session cookies." ], feature.operational_notes_for("8.0")
+    assert_equal [ "app/models/current.rb", "app/models/session.rb", "app/views/auth_labs/show.html.erb", "app/views/passwords/edit.html.erb" ], feature.files_for("8.1.2")
     assert_predicate feature, :live_demo_available?
   end
 

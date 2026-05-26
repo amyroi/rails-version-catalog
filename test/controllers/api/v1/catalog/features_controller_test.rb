b6e6b9bb62d64ab65fae8a9d202dd49f4a5b288d
Atsuko Mori <1243446+amyroi@users.noreply.github.com>
2026-05-26 20:38:18 +0900

    Add catalog feature detail API

--- test/controllers/api/v1/catalog/features_controller_test.rb
+++ test/controllers/api/v1/catalog/features_controller_test.rb
@@ -64,3 +64,66 @@ class Api::V1::Catalog::FeaturesControllerTest < ActionDispatch::IntegrationTest
       ].sort
     end
 end
+
+class Api::V1::Catalog::FeaturesControllerDetailTest < ActionDispatch::IntegrationTest
+  test "show returns feature detail as camelCase JSON" do
+    get api_v1_catalog_feature_url("solid-queue")
+
+    assert_response :success
+
+    feature = JSON.parse(response.body)
+    assert_equal "solid-queue", feature.fetch("slug")
+    assert_equal expected_detail_keys, feature.keys.sort
+    assert_equal "runtime_demo", feature.fetch("demoType")
+    assert_equal true, feature.fetch("liveDemoAvailable")
+    assert_kind_of Hash, feature.fetch("notesByVersion")
+    assert_kind_of Hash, feature.fetch("highlightsByVersion")
+    assert_kind_of Hash, feature.fetch("sourceLinksByVersion")
+    assert_kind_of Hash, feature.fetch("filesByVersion")
+    assert_kind_of Array, feature.fetch("adoptionWhen")
+  end
+
+  test "show returns JSON 404 for unknown slug" do
+    get api_v1_catalog_feature_url("no-such-feature")
+
+    assert_response :not_found
+    assert_equal({ "error" => "not_found" }, JSON.parse(response.body))
+  end
+
+  test "show does not include runtime demo record collections" do
+    get api_v1_catalog_feature_url("solid-queue")
+
+    assert_response :success
+
+    feature = JSON.parse(response.body)
+    refute_includes feature.keys, "queueRuns"
+    refute_includes feature.keys, "demoMessages"
+  end
+
+  private
+    def expected_detail_keys
+      %w[
+        adoptionAlternatives
+        adoptionCautions
+        adoptionRequirements
+        adoptionWhen
+        category
+        codeExamplesByVersion
+        demoType
+        filesByVersion
+        highlightsByVersion
+        latestHighlight
+        latestVersionKey
+        liveDemoAvailable
+        notesByVersion
+        operationalNotesByVersion
+        slug
+        sourceLinksByVersion
+        statusByVersion
+        summary
+        supportedVersions
+        title
+        upgradeNotesByVersion
+      ].sort
+    end
+end

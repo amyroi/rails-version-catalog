5d66d85da55646aec3309e31f59c6c4b635aa2d1
Atsuko Mori <1243446+amyroi@users.noreply.github.com>
2026-06-02 22:01:09 +0900

    Use current Rack status name in request spec

--- spec/requests/feature_catalog_flow_spec.rb
+++ spec/requests/feature_catalog_flow_spec.rb
@@ -94,7 +94,7 @@ RSpec.describe "FeatureCatalogFlow", type: :request do
         }
       }
 
-      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to have_http_status(:unprocessable_content)
       expect(response.body).to include("Email address can&#39;t be blank")
     end
 
